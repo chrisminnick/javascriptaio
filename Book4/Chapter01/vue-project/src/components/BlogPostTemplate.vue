ca4d9662b45b2f525b5ceccf7b82692c3981d7b0
Chris Minnick <chris@minnick.com>
2023-01-16 11:19:16 -0800

    cleaning up

--- Book4/Chapter01/vue-project/src/components/BlogPostTemplate.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <h1>
-    <slot name="title">No title provided</slot>
-  </h1>
-  <div>
-    <slot name="body">No body provided</slot>
-  </div>
-</template>
--- Book4/Chapter01/vue-project/src/components/ChildComponent.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>No they shouldn't!</h1>
-</template>
-
-<style>
-h1 {
-  border: none;
-}
-</style>
